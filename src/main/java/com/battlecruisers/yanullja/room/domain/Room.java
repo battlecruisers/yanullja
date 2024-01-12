e4458f97256532de75cacce6c65656f598e7eb4e
hyunwoo <hyunwoo0318@naver.com>
2024-01-12 11:10:22 +0900

    feat(Room, RoomInfo): connection room roomInfo Entity

--- src/main/java/com/battlecruisers/yanullja/room/domain/Room.java
+++ src/main/java/com/battlecruisers/yanullja/room/domain/Room.java
@@ -2,11 +2,15 @@ package com.battlecruisers.yanullja.room.domain;
 
 import com.battlecruisers.yanullja.base.BaseDate;
 import com.battlecruisers.yanullja.place.domain.Place;
+import com.battlecruisers.yanullja.roominfo.domain.RoomInfo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Room extends BaseDate {
@@ -19,6 +23,8 @@ public class Room extends BaseDate {
 
     private String category;
 
-    @ManyToOne
-    private Place place;
+    @ManyToOne private Place place;
+
+    @OneToMany(mappedBy = "room")
+    private List<RoomInfo> roomInfoList = new ArrayList<>();
 }
