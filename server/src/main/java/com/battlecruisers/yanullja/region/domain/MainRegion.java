e0cfa736181ceaf216963be2ffc064e349092758
hyunwoo <hyunwoo0318@naver.com>
2024-01-16 19:07:28 +0900

    feat(add-query-region): Add Region Query

--- server/src/main/java/com/battlecruisers/yanullja/region/domain/MainRegion.java
+++ server/src/main/java/com/battlecruisers/yanullja/region/domain/MainRegion.java
@@ -1,7 +1,6 @@
-package com.battlecruisers.yanullja.mainregion.domain;
+package com.battlecruisers.yanullja.region.domain;
 
 import com.battlecruisers.yanullja.base.BaseDate;
-import com.battlecruisers.yanullja.subregion.domain.SubRegion;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,16 +8,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MainRegion extends BaseDate {
 
+    @OneToMany(mappedBy = "mainRegion")
+    private final List<SubRegion> subRegionList = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
-  @OneToMany(mappedBy = "mainRegion")
-  private List<SubRegion> subRegionList = new ArrayList<>();
+    public MainRegion(String name) {
+        this.name = name;
+    }
 }
--- server/src/main/java/com/battlecruisers/yanullja/region/domain/SubRegion.java
+++ server/src/main/java/com/battlecruisers/yanullja/region/domain/SubRegion.java
@@ -1,7 +1,6 @@
-package com.battlecruisers.yanullja.subregion.domain;
+package com.battlecruisers.yanullja.region.domain;
 
 import com.battlecruisers.yanullja.base.BaseDate;
-import com.battlecruisers.yanullja.mainregion.domain.MainRegion;
 import com.battlecruisers.yanullja.place.domain.Place;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,19 +10,27 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubRegion extends BaseDate {
 
+    @OneToMany(mappedBy = "subRegion")
+    private final List<Place> placeList = new ArrayList<>();
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     @ManyToOne
     private MainRegion mainRegion;
 
-  @OneToMany(mappedBy = "subRegion")
-  private List<Place> placeList = new ArrayList<>();
+    public SubRegion(String name, MainRegion mainRegion) {
+        this.name = name;
+        this.mainRegion = mainRegion;
+    }
 }
