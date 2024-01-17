489e669ca49233dfb2c9786855bfa63f217bbdbd
venus01 <geumsung7769@naver.com>
2024-01-17 15:52:08 +0900

    refactor(MemberCouponService, Coupon) : MemberCouponService의 createMemberCoupon메서드에 HttpServletRequest가 매개변수로 들어가는 것을 수정, Coupon Entity에 @Getter 추가

---
+++ server/src/main/java/com/battlecruisers/yanullja/coupon/CouponService.java
@@ -0,0 +1,29 @@
+package com.battlecruisers.yanullja.coupon;
+
+import com.battlecruisers.yanullja.coupon.domain.Coupon;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CouponService {
+    private final CouponRepository couponRepository;
+
+    public CouponService(CouponRepository couponRepository){
+        this.couponRepository = couponRepository;
+    }
+
+    // 적용 가능한 전체 쿠폰 조회
+    public List<Coupon> getCouponList(){
+        return couponRepository.findAll();
+    }
+
+    // 하나의 쿠폰 정보 조회
+    public Coupon getCoupon(Long id){
+         Optional<Coupon> coupon = couponRepository.findById(id);
+
+        return coupon.orElseThrow();
+    }
+
+}
