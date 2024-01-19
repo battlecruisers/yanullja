a3d01fd132a309cbd75494ccedd0b7924b4c3aa8
venus01 <geumsung7769@naver.com>
2024-01-19 15:57:37 +0900

    refactor(MemberCouponRepository): clean up unnecessary code

--- server/src/main/java/com/battlecruisers/yanullja/coupon/MemberCouponRepository.java
+++ server/src/main/java/com/battlecruisers/yanullja/coupon/MemberCouponRepository.java
@@ -12,6 +12,5 @@ public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long
     // 회원의 쿠폰 사용내역 조회
     Optional<List<MemberCoupon>> findByMemberAndIsUsed(Member member, boolean isUsed);
 
-    // 회원이 쿠폰을 등록한 적이 있는지 조회(이미 등록한 쿠폰 중복 등록을 방지하기 위함)
-    Optional<Long> countByCouponAndMember(Coupon coupon, Member member);
+
 }
