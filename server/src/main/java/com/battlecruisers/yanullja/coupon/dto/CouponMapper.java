6573c7fb6065519f5e2e861eff7b7c84cbda9082
Daehyun Kim <kimdhyungg@gmail.com>
2024-01-24 19:34:53 +0900

    fix(coupon): build error using mapper

--- server/src/main/java/com/battlecruisers/yanullja/coupon/dto/CouponMapper.java
+++ server/src/main/java/com/battlecruisers/yanullja/coupon/dto/CouponMapper.java
@@ -1,10 +1,9 @@
 package com.battlecruisers.yanullja.coupon.dto;
 
 import com.battlecruisers.yanullja.coupon.domain.Coupon;
-import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(componentModel = "spring")
 public interface CouponMapper {
 
     CouponResponseDto toCouponResponseDto(Coupon coupon);
