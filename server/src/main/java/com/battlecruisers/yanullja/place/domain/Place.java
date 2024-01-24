bdc8f27b7714cd2210b762129cf8676a0d79d06c
hyunwoo <hyunwoo0318@naver.com>
2024-01-24 14:09:14 +0900

    feat(Place, MainRegion, SubRegion): add static contstructor

--- server/src/main/java/com/battlecruisers/yanullja/place/domain/Place.java
+++ server/src/main/java/com/battlecruisers/yanullja/place/domain/Place.java
@@ -9,14 +9,18 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Place extends BaseDate {
 
     @Id
@@ -32,7 +36,8 @@ public class Place extends BaseDate {
 
     private String address;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sub_region_id")
     private SubRegion subRegion;
 
     @OneToMany(mappedBy = "place", fetch = FetchType.LAZY)
@@ -43,4 +48,26 @@ public class Place extends BaseDate {
 
     @OneToMany(mappedBy = "place", fetch = FetchType.LAZY)
     private List<PlaceImage> placeImages = new ArrayList<>();
+
+    protected Place(String name, String category, String event, String policy, String address,
+        SubRegion subRegion, List<Room> roomList, List<Theme> themeList,
+        List<PlaceImage> placeImages) {
+        this.name = name;
+        this.category = category;
+        this.event = event;
+        this.policy = policy;
+        this.address = address;
+        this.subRegion = subRegion;
+        this.roomList = roomList;
+        this.themeList = themeList;
+        this.placeImages = placeImages;
+    }
+
+    public static Place createPlace(String name, String category, String event, String policy,
+        String address,
+        SubRegion subRegion, List<Room> roomList, List<Theme> themeList,
+        List<PlaceImage> placeImages) {
+        return new Place(name, category, event, policy, address, subRegion, roomList, themeList,
+            placeImages);
+    }
 }
