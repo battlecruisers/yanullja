ad0e2cf478d0ca7a7294e1e453570c28f118b93b
Daehyun Kim <kimdhyungg@gmail.com>
2024-01-26 09:19:55 +0900

    refactor(config): mistake that configs were inside logging directory

--- server/src/main/java/com/battlecruisers/yanullja/common/config/QueryDslConfig.java
+++ server/src/main/java/com/battlecruisers/yanullja/common/config/QueryDslConfig.java
@@ -1,4 +1,4 @@
-package com.battlecruisers.yanullja.common.logging.config;
+package com.battlecruisers.yanullja.common.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
