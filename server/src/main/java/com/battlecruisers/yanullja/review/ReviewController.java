0564407c8cf4088499904011b3675148e0df41bb
yukicow <simee625@naver.com>
2024-02-13 22:49:20 +0900

    feat(ReviewController): Add authPrincipal to review controller

--- server/src/main/java/com/battlecruisers/yanullja/review/ReviewController.java
+++ server/src/main/java/com/battlecruisers/yanullja/review/ReviewController.java
@@ -2,11 +2,13 @@ package com.battlecruisers.yanullja.review;
 
 
 import com.battlecruisers.yanullja.common.exception.CustomValidationException;
+import com.battlecruisers.yanullja.member.domain.SecurityMember;
 import com.battlecruisers.yanullja.review.dto.ReviewSaveDto;
 import com.battlecruisers.yanullja.review.example.WriteReviewOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,8 +26,7 @@ public class ReviewController {
 
     @WriteReviewOperation
     @PostMapping("/reviews")
-    public ResponseEntity<Object> writeReviews(
-        @Validated @RequestBody ReviewSaveDto saveForm, BindingResult bindingResult) {
+    public ResponseEntity<Object> writeReviews(@AuthenticationPrincipal SecurityMember member, @Validated @RequestBody ReviewSaveDto saveForm, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
             throw new CustomValidationException(
@@ -33,7 +34,7 @@ public class ReviewController {
                     .getDefaultMessage());
         }
 
-        reviewService.saveReview(saveForm, 1L);
+        reviewService.saveReview(saveForm, member.getId());
 
         return ResponseEntity
             .ok()
