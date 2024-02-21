d35a7478697e511cc69b911b7ff949df960127a2
venus01 <geumsung7769@naver.com>
2024-02-21 13:51:40 +0900

    feat(SwaggerExample,Add README) :  CouponController, MemberCouponController, CouponDto, MemberCouponDto 관련 Swagger 예시항목 추가, README 기술적도전항목 작성

--- server/src/main/java/com/battlecruisers/yanullja/coupon/CouponController.java
+++ server/src/main/java/com/battlecruisers/yanullja/coupon/CouponController.java
@@ -2,9 +2,6 @@ package com.battlecruisers.yanullja.coupon;
 
 import com.battlecruisers.yanullja.coupon.dto.CouponDto;
 import java.util.List;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,19 +19,16 @@ public class CouponController {
 
     private final CouponService couponService;
 
-    @Operation(summary = "전체 쿠폰 목록 조회")
     @GetMapping("")
     // 전체 쿠폰 목록 조회
     public ResponseEntity<List<CouponDto>> list() {
         List<CouponDto> test = couponService.getCouponList();
         return new ResponseEntity<>(test, HttpStatus.OK);
     }
-    //    @ApiResponses(value = {
-    //            @ApiResponse(responseCode = "200", description = "성공적인 조회")
-    //    })
-    @Operation(summary = "특정 쿠폰 조회")
+
     @GetMapping("/{couponId}")
     // 하나의 쿠폰 조회
+
     public ResponseEntity<CouponDto> coupon(
         @PathVariable(name = "couponId") Long id) {
         CouponDto couponDto = couponService.getCoupon(id);
